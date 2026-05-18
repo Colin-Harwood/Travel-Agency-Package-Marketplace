a1a55acc5b688e65b001ad0e7c15089cfb2ca156
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2026-05-18 12:56:17 +0200

    added the api key verification

--- backend/traveller/userApi.php
+++ backend/traveller/userApi.php
@@ -26,18 +26,46 @@ class Database {
 
         $request_data = json_decode($raw_json);
 
+        // check if there is the type field for routing
         if (!$request_data || !isset($request_data->type)) {
             $this->sendResponse("error", "Invalid JSON or missing 'type' field.", 400);
         }
 
+        // make sure api key field is specified
+        if (!isset($request_data->apikey)) {
+            $this->sendResponse("error", "Missing apikey field.", 400);
+        }
+
+        $stmt = $this->conn->prepare("SELECT userID from TRAVELLER where userID = ?");
+        $stmt->bind_param("s", $request_data->apikey);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows === 0) {
+            $this->sendResponse("error", "Invalid api key.", 409);
+        }
+
         if ($request_data->type === "getAllPackages") {
             $this->getAllPackages($request_data);
+        } elseif ($request_data->type === "getPackage") {
+            $this->getPackage($request_data);
         }
     }
 
-    function getAllPackages($request_data) {
+    function getAllPackages($data) {
         
     }
+
+    function getPackage($data) {
+        if (!isset($data->packageId)) {
+            $this->sendResponse("error", "Missing package id field", 400);
+        }
+
+        // $stmt = $this->conn->prepare
+    }
+
+
 
     private function sendResponse($status, $data, $http_code = 200) {
         http_response_code($http_code);
